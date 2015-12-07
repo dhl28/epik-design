d0fc5113c2208812bb97f26edaa583f9a1e5dafd
yanjia <yanjia@hanhua.com>
2015-12-07 15:26:20 +0800

    修改componenet chexkbox页面

--- client/app/component/comForm/comCheckbox/comCheckbox.controller.js
+++ client/app/component/comForm/comCheckbox/comCheckbox.controller.js
@@ -2,4 +2,7 @@
 
 angular.module('epikDesignApp')
   .controller('ComCheckboxCtrl', function ($scope) {
+    $scope.vm = {
+      data1 : 0
+    };
   });
--- client/app/component/component.controller.js
+++ client/app/component/component.controller.js
@@ -2,4 +2,5 @@
 
 angular.module('epikDesignApp')
   .controller('ComponentCtrl', function ($scope) {
+
   });
