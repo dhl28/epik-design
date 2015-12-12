b972828b73ff4a1a2f5f2cfabf5c8ad3aa71fcda
yanjia <yanjia@hanhua.com>
2015-12-12 17:58:20 +0800

    修改表单验证页面

--- client/app/comForm/comFormValidate/comFormValidate.controller.js
+++ client/app/comForm/comFormValidate/comFormValidate.controller.js
@@ -2,5 +2,16 @@
 
 angular.module('epikDesignApp')
   .controller('ComFormValidateCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    $scope.vm = {}
+
+    $scope.classifications = {
+      class1: [
+        {id: 1, name: '选项1', code: 'code1', path: 'a'},
+        {id: 2, name: '选项2', code: 'code2', path: 'b'},
+        {id: 3, name: '选项3', code: 'code3', path: 'c'},
+        {id: 4, name: '选项4', code: 'code4', path: 'd'},
+        {id: 5, name: '选项5', code: 'code5', path: 'e'},
+        {id: 6, name: '选项6', code: 'code6', path: 'f'}
+      ]
+    }
   });
